55d5cda53b3df5479c8532d1f27c7e4645707a98
sandeep <sandeep.palancha@starof.com>
2025-05-11 22:54:35 +0530

    back to workaround

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -190,7 +190,7 @@ function configureSocket(server) {
         }
         
         // Send push notification if recipient is offline
-        if (!recipientSocketId) {
+        if (!recipientSocketId || true) {
           // Also include attachment info in the notification
           const hasAttachments = messageWithAttachments.attachments.length > 0;
           const notificationMessage = hasAttachments 
